1baed690e028f63d4fdd1434e51e5b0a32b31fbf
DrVickie <116500226+drvickie@users.noreply.github.com>
2025-10-15 10:22:47 +0100

    Editing the h1

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React + Demo</h1>
       
       <p className="read-the-docs">
         This is a react demo for pushing to github.
